7f22e4c34fc5c716a2e20b04942583b6666fc7f7
njoseph <njoseph@bravurasolutions.com>
2022-09-05 09:32:02 +1000

    some changes for my setup

--- main.go
+++ main.go
@@ -2,39 +2,35 @@ package main
 
 import (
 	"crypto/rand"
+	"flag"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
-	"html/template"
 )
 
-const maxUploadSize = 2 * 1024 * 1024 // 2 mb
-const uploadPath = "./tmp"
-
 func main() {
-	http.HandleFunc("/upload", uploadFileHandler())
+	portPtr := flag.Int("p", 8080, "port number")
+	sharedFolder := flag.String("f", ".", "local folder path for upload and download")
+	uploadPath := *sharedFolder
+	port := *portPtr
+	http.HandleFunc("/upload", uploadFileHandler(uploadPath))
 
 	fs := http.FileServer(http.Dir(uploadPath))
 	http.Handle("/files/", http.StripPrefix("/files", fs))
 
-	log.Print("Server started on localhost:8080, use /upload for uploading files and /files/{fileName} for downloading")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Server started on %d, use /upload for uploading files and /files/{fileName} for downloading", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprint(":", port), nil))
 }
 
-func uploadFileHandler() http.HandlerFunc {
+func uploadFileHandler(uploadPath string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			t, _ := template.ParseFiles("upload.gtpl")
 			t.Execute(w, nil)
-			return
-		}
-		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
-			fmt.Printf("Could not parse multipart form: %v\n", err)
-			renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
 			return
 		}
 
@@ -44,40 +40,16 @@ func uploadFileHandler() http.HandlerFunc {
 			renderError(w, "INVALID_FILE", http.StatusBadRequest)
 			return
 		}
+		fmt.Printf("FILE NAME : %s", fileHeader.Filename)
 		defer file.Close()
-		// Get and print out file size
-		fileSize := fileHeader.Size
-		fmt.Printf("File size (bytes): %v\n", fileSize)
-		// validate file size
-		if fileSize > maxUploadSize {
-			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-			return
-		}
 		fileBytes, err := ioutil.ReadAll(file)
 		if err != nil {
 			renderError(w, "INVALID_FILE", http.StatusBadRequest)
 			return
 		}
 
-		// check file type, detectcontenttype only needs the first 512 bytes
-		detectedFileType := http.DetectContentType(fileBytes)
-		switch detectedFileType {
-		case "image/jpeg", "image/jpg":
-		case "image/gif", "image/png":
-		case "application/pdf":
-			break
-		default:
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
-		fileName := randToken(12)
-		fileEndings, err := mime.ExtensionsByType(detectedFileType)
-		if err != nil {
-			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-			return
-		}
-		newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
-		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+		newPath := filepath.Join(uploadPath, fileHeader.Filename)
+		//fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
 
 		// write file
 		newFile, err := os.Create(newPath)
--- upload.gtpl
+++ upload.gtpl
@@ -7,5 +7,6 @@
 	<input type="file" name="uploadFile" />
 	<input type="submit" value="upload" />
 </form>
+<button onclick="location.href = '/files';" id="myButton" class="float-left submit-button" >View files</button>
 </body>
 </html>
